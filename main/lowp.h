aaac2e52fc82e79249c677dbfbb3e384bdb32107
Guilherme Gazzo <guilhermegazzo@gmail.com>
2016-06-09 20:57:43 -0300

    done , mas estourou o limite =/

---
+++ main/lowp.h
@@ -0,0 +1,42 @@
+#define __AVR_ATtiny85__
+#include <avr/sleep.h>
+#include <avr/wdt.h>
+
+#ifndef cbi
+#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
+#endif
+#ifndef sbi
+#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
+#endif
+
+
+#define setup_sleep setup_watchdog
+char f_wdt = 0;
+
+
+// set system into the sleep state
+// system wakes up when wtchdog is timed out
+#define system_sleep() cbi(ADCSRA,ADEN);\
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);\
+  sleep_enable();\
+  sleep_mode();\
+  sleep_disable();\
+  sbi(ADCSRA,ADEN);
+
+#define sleep(); while (f_wdt < 224) {\
+    system_sleep();\
+  }\
+  f_wdt = 0;
+// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
+// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
+#define setup_watchdog()  byte bb;char ww; bb=9 & 7;   bb|= (1<<5);  bb|= (1<<WDCE);  ww=bb;\
+  MCUSR &= ~(1<<WDRF);\
+  WDTCR |= (1<<WDCE) | (1<<WDE);\
+  WDTCR = bb;\
+  WDTCR |= _BV(WDIE);
+
+
+// Watchdog Interrupt Service / is executed when watchdog timed out
+ISR(WDT_vect) {
+  f_wdt++;  // set global flag
+}
--- main/main.ino
+++ main/main.ino
@@ -1,106 +1,100 @@
-
-/*
- * Watchdog Sleep Example 
- * Demonstrate the Watchdog and Sleep Functions
- * LED on digital pin 0
- * 
- * KHM 2008 / Lab3/  Martin Nawrath nawrath@khm.de
- * Kunsthochschule fuer Medien Koeln
- * Academy of Media Arts Cologne
- *
- * Modified on 5 Feb 2011 by InsideGadgets (www.insidegadgets.com)
- * to suit the ATtiny85 and removed the cbi( MCUCR,SE ) section 
- * in setup() to match the Atmel datasheet recommendations
- */
-#define __AVR_ATtiny85__
-#include <avr/sleep.h>
-#include <avr/wdt.h>
-
+#include "lowp.h"
 #include <Wire.h>
 
-#ifndef cbi
-#define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
-#endif
-#ifndef sbi
-#define sbi(sfr, bit) (_SFR_BYTE(sfr) |= _BV(bit))
-#endif
-int acelX,acelY,acelZ,temperatura,giroX,giroY,giroZ;
-int pinLed = 0;
-int f_wdt = 30*60/8;
+
+#include <SoftSerial.h>
+
+SoftSerial mySerial(2, 3);
+
+
 const int MPU=0x68;
+
+//#define PIN_DEBUG 1
+
+/* IMU Data */
+int accX, accY, accZ;
+int gyroX, gyroY, gyroZ;
+int tempRaw;
+
+float gyroXrate, gyroYrate;
+
+
+uint32_t timer, dt;
+
+
+float roll, pitch;
+float roll_final, pitch_final;
+float roll_new, pitch_new;
+inline void read_data(){
+  Wire.beginTransmission(MPU);      //transmite
+  Wire.write(0x3B);                 // Endereço 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);     //Finaliza transmissão
+
+  Wire.requestFrom(MPU,14);   //requisita bytes
+  // Armazena o valor dos sensores nas variaveis correspondentes
+  accX=Wire.read()<<8|Wire.read();  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  accY=Wire.read()<<8|Wire.read();  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  accZ=Wire.read()<<8|Wire.read();  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  tempRaw=Wire.read()<<8|Wire.read();  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  gyroX=Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  gyroY=Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  gyroZ=Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+}
+
+void first(){
+  read_data();
+  dt = (float)(micros() - timer) / 1000000;
+  timer = micros();
+  get_roll_pitch();
+}
+
+#define complementary(); \
+  if ((roll < -90 && roll_new > 90) || (roll > 90 && roll_new < -90)) {\
+    roll = roll_new;\
+    roll = 0.93 * (roll + gyroXrate * dt) + 0.07 * roll;\
+  }\
+  if (abs(roll) > 90)\
+    gyroYrate = -gyroYrate;\
+  pitch = 0.93 * (pitch + gyroYrate * dt) + 0.07 * pitch;
+
+
+/* Some example here */
+
 void setup(){
-  pinMode(pinLed,OUTPUT);
-  setup_watchdog(9); // approximately 8 seconds sleep
-  Wire.begin();                 //inicia I2C
-  Wire.beginTransmission(MPU);  //Inicia transmissão para o endereço do MPU
-  Wire.write(0x6B);  
-  Wire.write(0); 
-  Wire.endTransmission(true);
+
+  mySerial.begin(4800);
+  setup_sleep();
+  // Wire.begin();                 //inicia I2C
+  // Wire.beginTransmission(MPU);  //Inicia transmissão para o endereço do MPU
+  // Wire.write(0x6B);
+
+  // Wire.write(0);
+  // Wire.endTransmission(true);
+}
+
+#define request_data(void);  first ();\
+  gyroXrate = gyroX / 131.0;\
+  gyroYrate = gyroY / 131.0;\
+  complementary();
+//complementary(&roll,&pitch,&roll_new, &gyroXrate, &gyroYrate);
+
+//complementary(&pitch,&roll,&pitch_new, &gyroYrate, &gyroXrate);\
+
+
+void  get_roll_pitch() {
+  roll_new  = atan2(accY, accZ) * RAD_TO_DEG;
+  pitch_new = atan(-accX / sqrt(accY * accY + accZ * accZ)) * RAD_TO_DEG;
 }
 
 void loop(){
-  
-
-    Wire.beginTransmission(MPU);      //transmite
-    Wire.write(0x3B);                 // Endereço 0x3B (ACCEL_XOUT_H)
-    Wire.endTransmission(false);     //Finaliza transmissão
-    
-    Wire.requestFrom(MPU,14);   //requisita bytes
-     
-    //Armazena o valor dos sensores nas variaveis correspondentes
-    acelX=Wire.read()<<8|Wire.read();  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
-    acelY=Wire.read()<<8|Wire.read();  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-    acelZ=Wire.read()<<8|Wire.read();  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-    temperatura=Wire.read()<<8|Wire.read();  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-    giroX=Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-    giroY=Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-    giroZ=Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-    ds();
-}
-
-
-inline void ds (){
-  while (f_wdt < 224) {
-    system_sleep();    
-  }
-  f_wdt = 0;
-}
-// set system into the sleep state 
-// system wakes up when wtchdog is timed out
-void system_sleep() {
-  cbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter OFF
-
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
-  sleep_enable();
-
-  sleep_mode();                        // System sleeps here
-
-  sleep_disable();                     // System continues execution here when watchdog timed out 
-  sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
-}
-
-// 0=16ms, 1=32ms,2=64ms,3=128ms,4=250ms,5=500ms
-// 6=1 sec,7=2 sec, 8=4 sec, 9= 8sec
-void setup_watchdog(int ii) {
-
-  byte bb;
-  int ww;
-  if (ii > 9 ) ii=9;
-  bb=ii & 7;
-  if (ii > 7) bb|= (1<<5);
-  bb|= (1<<WDCE);
-  ww=bb;
-
-  MCUSR &= ~(1<<WDRF);
-  // start timed sequence
-  WDTCR |= (1<<WDCE) | (1<<WDE);
-  // set new watchdog timeout value
-  WDTCR = bb;
-  WDTCR |= _BV(WDIE);
-}
-  
-// Watchdog Interrupt Service / is executed when watchdog timed out
-ISR(WDT_vect) {
-  f_wdt++;  // set global flag
+    first ();
+    do {
+      roll_final = roll =  roll_new;
+      pitch_final = pitch =  pitch_new;
+      request_data();
+    } while(--a > 0);
+    mySerial.print(roll_final);
+    mySerial.print(pitch_final);
+    sleep();
 }
